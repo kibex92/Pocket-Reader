8c69f006130d54c1fee3d3e2efab24b0a5b5f66d
Boris Knezevic <boris.knezevic0@gmail.com>
2021-04-26 17:13:59 +0200

    Add view spec

---
+++ spec/view_spec.rb
@@ -0,0 +1,35 @@
+require_relative 'reader_helper'
+
+begin
+  require 'post_factory'
+  require 'view'
+rescue LoadError
+end
+
+reader_helper = ReaderHelper.new(
+  file_name: 'view',
+  class_name: 'View'
+)
+
+describe 'View', unless: reader_helper.file_and_class_valid? do
+  it '`view.rb` file should exist' do
+    expect(reader_helper.file_exists?).to be(true)
+  end
+
+  it 'View class should be defined' do
+    expect(reader_helper.class_defined?).to be(true)
+  end
+end
+
+describe 'View', if: reader_helper.file_and_class_valid? do
+  let(:view) { View.new() }
+
+  describe '#display' do
+    it 'should display posts' do
+      posts = []
+      posts << PostFactory.build('author', 'title', 'content')
+      expect(STDOUT).to receive(:puts).with(/.*(title).*/)
+      view.display(posts)
+    end
+  end
+end
